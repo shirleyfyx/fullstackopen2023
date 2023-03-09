9c105748f47743bd275582a0bf477bf52637d5cb
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-09 13:51:28 -0500

    more courses added

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Course from'./course.js';
 
 function ExerciseSum({parts}) {
-  const sum = Course.reduce((accumulator, currentValue) => accumulator+currentValue.exercises, 0);
+  const sum = parts.reduce((accumulator, currentValue) => accumulator+currentValue.exercises, 0);
   return (
     <div>
       <strong><p></p>total of {sum} exercises</strong>
@@ -10,18 +10,33 @@ function ExerciseSum({parts}) {
   )
 }
 
+function Part ({part}) {
+  return (
+    <div key = {part.id}>
+      <p>{part.name} {' '} {part.exercises}</p>
+    </div>
+  )
+}
+
+function Courses({course}) {
+  return (
+    <div> 
+      <h2>{course.name}</h2>
+      {course.parts.map(part =>
+        <Part key = {part.id} part = {part} />
+      )}
+      <ExerciseSum parts = {course.parts} />
+    </div>
+  )
+}
+
 const App = () => {
-  console.log(Course)
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {Course.map(part =>
-          <div key={part.id}>
-            <p></p>{part.name} 
-            {' '}{part.exercises}
-          </div>
-        )}
-        <ExerciseSum />
+      {Course.map(course => 
+      <Courses key={course.id} course = {course} />
+      )}
     </div>
   )
 }
