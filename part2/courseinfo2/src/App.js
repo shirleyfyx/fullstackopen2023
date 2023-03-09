c536fbc298614d6afc43693ab231fd750b89cf25
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-09 12:14:31 -0500

    an updated version of courseinfo

---
+++ part2/courseinfo2/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Course from'./course.js';
+
+function ExerciseSum({parts}) {
+  const sum = parts.reduce((accumulator, currentValue) => accumulator+currentValue.exercises, 0);
+  return (
+    <div>
+      <strong><p></p>total of {sum} exercises</strong>
+    </div>
+  )
+}
+
+function Part ({part}) {
+  return (
+    <div key = {part.id}>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+function Courses({course}) {
+  return (
+    <div>
+      <h2>{course.name}</h2>{course.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+      <ExerciseSum parts={course.parts} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {Course.map(course =>
+        <Courses key={course.id} course={course} />
+      )}
+    </div>
+  )
+}
+
+export default App;
---
+++ part2/courseinfo2/src/course.js
@@ -0,0 +1,44 @@
+const Course = [
+    {
+        name:<strong>Half Stack application development</strong>,
+        id: 1,
+        parts: [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+        },
+        {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+        }
+        ]
+    }, 
+    {
+        name: <strong>Node.js</strong>,
+        id: 2,
+        parts: [
+        {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+        },
+        {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+        }
+        ]
+    }]
+      export default Course;  
