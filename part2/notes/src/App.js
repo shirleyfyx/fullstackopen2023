8d1fbf94b142f0d47d57f5e5a2f73092b49c0fa0
Shirley Fang <fangyixuan0123@gmail.com>
2023-03-11 23:45:26 -0500

    web application for adding new notes

---
+++ part2/notes/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Note from'./Note.js'
+
+
+const App = (props) => {
+  const [notes, setNotes ] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note}/>
+        )}
+      </ul>
+      <form onSubmit = {addNote}>
+        <input value = {newNote}
+        onChange = {handleNoteChange}/>
+        <button type="submit"> save </button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
