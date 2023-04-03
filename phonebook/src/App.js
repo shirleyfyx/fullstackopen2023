549e2b8615501874697025680285983fe6083e84
Shirley Fang <fangyixuan0123@gmail.com>
2023-04-03 16:07:43 -0400

    Created a webpage to upload names.

---
+++ phonebook/src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+        <li key={index}>{person.name}</li>
+      ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
