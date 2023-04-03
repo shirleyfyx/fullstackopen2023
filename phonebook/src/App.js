e3c4899483974a76e3f220979b57c87040618807
Shirley Fang <fangyixuan0123@gmail.com>
2023-04-03 19:23:51 -0400

    Created an input box to submit numbers.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,18 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Shirley Fang' }
+    { name: 'Shirley Fang',
+      number: '000-000-000'}
   ]) 
   const [newName, setNewName] = useState('Add Name...')
+  const [newNumber, setNewNumber] = useState('Add Number...')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     
     if (persons.some(person => person.name === newName)){
@@ -22,6 +29,7 @@ const App = () => {
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -32,13 +40,16 @@ const App = () => {
           name: <input value={newName} onChange = {handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange = {handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, index) => (
-        <li key={index}>{person.name}</li>
+        <li key={index}>{person.name} {person.number} </li>
       ))}
       </ul>
     </div>
