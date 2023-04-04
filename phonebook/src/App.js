5cf7b4f96988ac8ed83b387e6fb643c070435a82
Shirley Fang <fangyixuan0123@gmail.com>
2023-04-04 00:57:48 -0400

    Created a filter search input using filter function.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,10 +3,20 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Shirley Fang',
-      number: '000-000-000'}
+      number: '000-000-000'},
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('Add Name...')
   const [newNumber, setNewNumber] = useState('Add Number...')
+  const [filter, setFilter] = useState('')
+  
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -23,7 +33,7 @@ const App = () => {
       number: newNumber
     }
     
-    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())){
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase)){
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -32,9 +42,17 @@ const App = () => {
     setNewNumber('')
   }
 
+  var peopleToDisplay = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange}/>
+      </div>
+      
+      <h2>add a new</h2>
       <form onSubmit = {addPerson}>
         <div>
           name: <input value={newName} onChange = {handleNameChange}/>
@@ -48,7 +66,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
+        {
+        peopleToDisplay.map((person, index) => (
         <li key={index}>{person.name} {person.number} </li>
       ))}
       </ul>
