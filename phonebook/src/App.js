9cbcf511c0f90e34e708aaa77b57bb2696518f4f
Shirley Fang <fangyixuan0123@gmail.com>
2023-04-13 11:18:48 -0400

    Fetched the data from the server.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
 
@@ -62,16 +63,19 @@ const Display = ({peopleToDisplay}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Shirley Fang', number: '000-000-000'},
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('Add Name...')
   const [newNumber, setNewNumber] = useState('Add Number...')  
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
 
   var peopleToDisplay = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
--- phonebook/src/index.js
+++ phonebook/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import axios from 'axios'
 import App from './App';
 
 
@@ -10,6 +10,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const promise = axios.get('http://localhost:3001/persons')
+promise.then(response => {
+  console.log(response)
+})
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
