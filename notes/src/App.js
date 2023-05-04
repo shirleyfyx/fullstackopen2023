29b51733fc2383d9cdad89375b153553fc7f8992
Shirley Fang <fangyixuan0123@gmail.com>
2023-05-04 16:34:45 -0400

    Created an error message for any deleted notes.

--- notes/src/App.js
+++ notes/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -29,9 +29,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
